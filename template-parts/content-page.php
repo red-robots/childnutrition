590451d9575cc182ce9dcb2e30cd8b6ebbabfb86
Fritz <fre.healy@gmail.com>
2017-07-07 16:36:05 -0400

    major and minor styling page template

--- template-parts/content-page.php
+++ template-parts/content-page.php
@@ -9,6 +9,46 @@
 
 ?>
 
-<article id="post-<?php the_ID(); ?>" <?php post_class("template-page"); ?>>
-
+<article id="post-<?php the_ID(); ?>" <?php post_class("template-page clear-bottom"); ?>>
+    <div class="col-1">
+        <header><h1><?php the_title();?></h1></header>
+        <div class="copy">
+            <?php the_content();?>
+        </div><!--.copy-->
+    </div><!--.col-1-->
+    <?php $parent_id = wp_get_post_parent_id($id);
+    $args = array(
+        'post_type'=>'page',
+        'post_parent'=>$id,
+        'posts_per_page'=>-1,
+        'order'=>'ASC',
+        'orderby'=>'menu_order'
+    );
+    $query = new WP_Query($args);
+    if(!$query->have_posts()):
+        if($parent_id):
+            $args = array(
+                'post_type'=>'page',
+                'post_parent'=>$parent_id,
+                'posts_per_page'=>-1,
+                'order'=>'ASC',
+                'orderby'=>'menu_order'
+            );
+            $query = new WP_Query($args);
+            if(!$query->have_posts()):
+                $query = null;
+            endif;
+        else:
+            $query = null;
+        endif;
+    endif;?>
+    <?php if($query):?>
+        <div class="col-2">
+            <ul>
+                <?php while($query->have_posts()):$query->the_post();?>
+                    <li><i class="fa fa-caret-right"></i><a href="<?php echo get_the_permalink();?>"><?php the_title();?></a></li>
+                <?php endwhile;?>
+            </ul>
+        </div><!--.col-2-->
+    <?php endif;?>
 </article><!-- #post-## -->

a03c6237a8f1b9baa9b69a975399102167c75312
Fritz <fre.healy@gmail.com>
2018-01-03 10:52:49 -0500

    styling donate page

--- template-parts/content-donate.php
+++ template-parts/content-donate.php
@@ -17,40 +17,17 @@
                 <?php the_content();?>
             </div><!--.copy-->
         </section><!--.col-1-->
-        <?php $parent_id = wp_get_post_parent_id($id);
-        $args = array(
-            'post_type'=>'page',
-            'post_parent'=>$id,
-            'posts_per_page'=>-1,
-            'order'=>'ASC',
-            'orderby'=>'menu_order'
-        );
-        $query = new WP_Query($args);
-        if(!$query->have_posts()):
-            if($parent_id):
-                $args = array(
-                    'post_type'=>'page',
-                    'post_parent'=>$parent_id,
-                    'posts_per_page'=>-1,
-                    'order'=>'ASC',
-                    'orderby'=>'menu_order'
-                );
-                $query = new WP_Query($args);
-                if(!$query->have_posts()):
-                    $query = null;
-                endif;
-            else:
-                $query = null;
-            endif;
-        endif;?>
-        <?php if($query):?>
-            <aside class="col-2">
-                <ul>
-                    <?php while($query->have_posts()):$query->the_post();?>
-                        <li class="clear-bottom"><i class="fa fa-caret-right"></i><a href="<?php echo get_the_permalink();?>"><?php the_title();?></a></li>
-                    <?php endwhile;?>
-                </ul>
-            </aside><!--.col-2-->
-        <?php endif;?>
+        <aside class="col-2">
+            <?php $copy = get_field("sidebar_copy");
+            $image = get_field("sidebar_photo");
+            if($image):?>
+                <img src="<?php echo $image['sizes']['large'];?>" alt="<?php echo $image['alt'];?>">
+            <?php endif;
+            if($copy):?>
+                <div class="copy">
+                    <?php echo $copy;?>
+                </div><!--.copy-->
+            <?php endif;?>
+        </aside><!--.col-2-->
     </div><!--.row-2-->
 </article><!-- #post-## -->
